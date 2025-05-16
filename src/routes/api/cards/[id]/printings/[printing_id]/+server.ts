a3b7c5fa9bf209cbb19bad2b4f3a8fd834ba1a26
Alexandre Desbiens <linkalex8@hotmail.com>
2025-05-16 17:16:48 -0400

    Fixes

--- src/routes/api/cards/[id]/printings/[printing_id]/+server.ts
+++ src/routes/api/cards/[id]/printings/[printing_id]/+server.ts
@@ -3,6 +3,9 @@ import { json } from '@sveltejs/kit';
 
 export async function PUT({ request, params }) {
 	const body = await request.json();
-	await sql`update cards_printings set rarity = ${body.rarity} where id = ${params.printing_id}`;
+	await sql`update cards_printings set
+		rarity = ${body.rarity},
+		body = ${body.body}
+		where id = ${params.printing_id}`;
 	return json({});
 }
